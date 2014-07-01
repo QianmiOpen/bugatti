d37a16cd0963e668c8f00c90a44fdd69842646e5
of729 <jinwei@ofcard.com>
2014-07-01 15:32:20 +0800

    重构：区分taskActor 和 socketActor；

---
+++ app/models/task/SocketActor.scala
@@ -0,0 +1,45 @@
+package models.task
+
+import akka.actor._
+import play.api.libs.iteratee._
+import play.api.libs.json.JsValue
+import play.api.Logger
+
+
+/**
+ * 处理所有的websocket
+ * Created by jinwei on 1/7/14.
+ */
+class SocketActor extends Actor {
+
+  val (out, channel) = Concurrent.broadcast[JsValue]
+
+  def receive = {
+    case JoinProcess(js) => {
+      sender ! ConnectedSocket(out)
+      notifyAllSocket(js)
+    }
+    case QuitProcess() => {
+      Logger.info("有一个客户端关闭了连接")
+    }
+    case AllTaskStatus() => {
+      notifyAllSocket(TaskProcess.getAllStatus)
+    }
+  }
+
+  def notifyAllSocket(js: JsValue) {
+    Logger.info(js.toString())
+    Thread.sleep(100)
+    channel.push(js)
+  }
+}
+
+case class JoinProcess(js: JsValue)
+
+case class AllTaskStatus()
+
+case class ConnectedSocket(out: Enumerator[JsValue])
+
+case class CannotConnect(msg: String)
+
+case class QuitProcess()
