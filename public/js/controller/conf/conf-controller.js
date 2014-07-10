e5302a692ac7ddfc462a6ff2acbfca6f282f807e
mysoko <liyunheng@gmail.com>
2014-07-10 10:45:41 +0800

    code: 1.一键拷贝，判断版本是否存在

--- public/js/controller/conf/conf-controller.js
+++ public/js/controller/conf/conf-controller.js
@@ -84,10 +84,10 @@ define(['angular'], function(angular) {
     var ModalInstanceCtrl = function ($scope, $modalInstance, envs, versions, curr_eid, curr_vid) {
 
         $scope.envs = envs;
-        $scope.env = $scope.envs[0].id;
+        if ($scope.envs.length) $scope.env = $scope.envs[0].id;
 
         $scope.versions = versions;
-        $scope.version = $scope.versions[0].id;
+        if ($scope.versions.length) $scope.version = $scope.versions[0].id;
 
         $scope.override = false;
 
