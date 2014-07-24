a7a910a8d8fc55768b74ca4db409a0c95be7fe61
of729 <jinwei@ofcard.com>
2014-07-24 09:43:45 +0800

    修改SocketActor定时推送的调试信息为dubug级别；

--- app/actor/task/SocketActor.scala
+++ app/actor/task/SocketActor.scala
@@ -31,7 +31,7 @@ class SocketActor extends Actor{
   }
 
   def notifyAllSocket(js: JsValue) {
-    Logger.info("notifyAllSocket==>" + js.toString())
+    Logger.debug("notifyAllSocket==>" + js.toString())
     channel.push(js)
     //test
 //    val _baseLogPath = SaltTools.logPath
