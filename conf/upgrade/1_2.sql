86009945637466956e4e3cb74523b45092c3d960
彭毅 <pengyi@ofcard.com>
2014-07-26 15:13:39 +0800

    修改升级脚本

--- conf/upgrade/1_2.sql
+++ conf/upgrade/1_2.sql
@@ -16,11 +16,6 @@ ALTER TABLE `environment` ADD COLUMN `script_version` varchar(254) NOT NULL DEFA
 ALTER TABLE `environment_project_rel` DROP INDEX `idx_eid_pid`;
 ALTER TABLE `environment_project_rel` DROP INDEX `idx_ip`;
 
-ALTER TABLE `logging_event` DROP INDEX `idx_fulltext`;
-ALTER TABLE `logging_event` CHANGE COLUMN `timestmp` `timestmp` bigint(20) NOT NULL first;
-ALTER TABLE `logging_event` ADD INDEX `idx_time` USING BTREE (`timestmp`);
-ALTER TABLE `logging_event` ADD FULLTEXT `idx_fulltext` (`formatted_message`);
-
 CREATE TABLE `script_version` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(254) NOT NULL,
@@ -33,3 +28,41 @@ ALTER TABLE `task_template` ADD COLUMN `script_version` varchar(254) NOT NULL DE
 ALTER TABLE `template_item` DROP INDEX `idx_name`;
 ALTER TABLE `template_item` ADD COLUMN `script_version` varchar(254) NOT NULL DEFAULT 'master' after `order`;
 ALTER TABLE `template_item` ADD UNIQUE `idx_name` USING BTREE (`template_id`, `item_name`, `script_version`);
+
+DROP TABLE `logging_event`;
+CREATE TABLE `logging_event` (
+  `timestmp` varchar(20) NOT NULL,
+  `formatted_message` text NOT NULL,
+  `logger_name` varchar(254) NOT NULL,
+  `level_string` varchar(254) NOT NULL,
+  `thread_name` varchar(254) DEFAULT NULL,
+  `reference_flag` smallint(6) DEFAULT NULL,
+  `arg0` varchar(254) DEFAULT NULL,
+  `arg1` varchar(254) DEFAULT NULL,
+  `arg2` varchar(254) DEFAULT NULL,
+  `arg3` varchar(254) DEFAULT NULL,
+  `caller_filename` varchar(254) NOT NULL,
+  `caller_class` varchar(254) NOT NULL,
+  `caller_method` varchar(254) NOT NULL,
+  `caller_line` char(4) NOT NULL,
+  `event_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`event_id`),
+  FULLTEXT KEY `idx_fulltext` (`formatted_message`, `timestmp`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+DROP TABLE `logging_event_exception`;
+CREATE TABLE `logging_event_exception` (
+  `event_id` bigint(20) NOT NULL,
+  `i` smallint(6) NOT NULL,
+  `trace_line` varchar(254) NOT NULL,
+  PRIMARY KEY (`event_id`,`i`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+DROP TABLE `logging_event_property`;
+CREATE TABLE `logging_event_property` (
+  `event_id` bigint(20) NOT NULL,
+  `mapped_key` varchar(254) NOT NULL,
+  `mapped_value` text,
+  PRIMARY KEY (`event_id`,`mapped_key`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
