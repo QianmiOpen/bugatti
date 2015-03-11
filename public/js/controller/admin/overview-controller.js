6414b97abaed12d20d53ea6d78b562ebb1023d37
mysoko <liyunheng@gmail.com>
2015-03-11 15:46:43 +0800

    管理功能，控制面板完成

--- public/js/controller/admin/overview-controller.js
+++ public/js/controller/admin/overview-controller.js
@@ -3,8 +3,19 @@
 define(['angular'], function(angular){
     var app = angular.module('bugattiApp.controller.admin.overviewModule', []);
 
-    app.controller('OverviewCtrl', ['$scope', function($scope) {
-        console.log(1)
+    app.controller('OverviewCtrl', ['$scope', 'ProjectService', 'EnvService', 'UserService',
+        function($scope, ProjectService, EnvService, UserService) {
+        ProjectService.count('', false, function(data) {
+            $scope.projectCount = data;
+        });
+
+        EnvService.count(function(data) {
+            $scope.envCount = data;
+        });
+
+        UserService.count('', function(data) {
+            $scope.userCount = data;
+        });
     }]);
 
 });
